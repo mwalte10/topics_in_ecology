a250c2a8f6baa657f3ad943d2ac101ff7522fcc1
mwalte10 <mwalte10@nd.edu>
2018-05-26 15:24:50 -0400

    changed vac_l to zero

--- model_travel_null.R
+++ model_travel_null.R
@@ -6,8 +6,8 @@ args = commandArgs(TRUE)
 input = as.numeric(args[1])
 beta_h <- 0.3236842
 beta_l <- 0.95
-vac_h <- rep(0, 5)
-vac_l <- seq(0.1, 0.9, length.out = 5)
+vac_h <- seq(0.1, 0.9, length.out = 5)
+vac_l <- rep(0, 5)
 vac_h.new <- rep(vac_h, 5)
 vac_l.new <- rep(NA, 25)
 for(i in 1:5){
