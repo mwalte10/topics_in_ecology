0ad3628eab4a69e5f84742496aec78b459a90098
mwalte10 <mwalte10@nd.edu>
2019-01-28 17:39:32 -0500

    changed so that there is zero travel

--- model_dec_23.R
+++ model_dec_23.R
@@ -10,7 +10,7 @@ input = as.numeric(args[1])
 #IF DOING VAC COVERAGE
 ##########################
 load("parms.mat.dec_16.RData")
-parse <- which(new.parms.mat[,3] == seq(0, 0.05, length.out = 20)[11])
+parse <- which(new.parms.mat[,3] == seq(0, 0.05, length.out = 20)[1])
 new.parms.mat <- new.parms.mat[parse,]
 redo <- c(6, 11, 15, 16)
 input <- redo[input]
