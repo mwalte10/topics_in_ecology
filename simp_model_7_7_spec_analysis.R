c6efc2ee091407de2405df56515bb2be3b7f3765
mwalte10 <mwalte10@nd.edu>
2019-07-31 22:46:05 -0400

    .

--- simp_model_7_7_spec_analysis.R
+++ simp_model_7_7_spec_analysis.R
@@ -1708,8 +1708,8 @@ cases_vac.h <- sum((out.h[nrow(out.h),which(colnames(out.h) == 'sec_vac.cases.h'
 
 
 ##vaccinated, low
-cases_vac.l <- sum((out_mat[nrow(out.h),which(colnames(out.h) == 'sec_vac.cases.l')]),
-                   (out_mat[nrow(out.h),which(colnames(out.h) == 'psec_vac.cases.l')]))
+cases_vac.l <- sum((out.h[nrow(out.h),which(colnames(out.h) == 'sec_vac.cases.l')]),
+                   (out.h[nrow(out.h),which(colnames(out.h) == 'psec_vac.cases.l')]))
 
 cases_vac <- cases_vac.h + cases_vac.l
 
